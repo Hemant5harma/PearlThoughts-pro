09bdf1346cdd280c052129f493fbde0cac47f5c0
hemant5harma <hemantsharmahare@gmail.com>
2025-07-13 14:56:14 +0000

    govind

--- terraform/secret_manager.tf
+++ terraform/secret_manager.tf
@@ -6,8 +6,8 @@ resource "aws_secretsmanager_secret_version" "medusa_env_version" {
   secret_id     = aws_secretsmanager_secret.medusa_env.id
   secret_string = jsonencode({
     DB_PASSWORD = var.db_password,
-    ADMIN_CORS  = "http://localhost:7000",
-    STORE_CORS  = "http://localhost:8000",
-    AUTH_CORS   = "http://localhost:9000",
+    ADMIN_CORS  = "http://localhost:5173,http://localhost:9000,https://docs.medusajs.com",
+    STORE_CORS  = "http://localhost:8000,https://docs.medusajs.com",
+    AUTH_CORS   = "http://localhost:5173,http://localhost:9000,http://localhost:8000,https://docs.medusajs.com",
   })
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ""
+      version = "6.3.0"
     }
   }
 }
