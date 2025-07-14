4458090aa35cd3d48bcd1052de9cc69d37a14a98
hemant5harma <hemantsharmahare@gmail.com>
2025-07-14 08:02:21 +0000

    shayam

--- terraform-app/ecr.tf
+++ terraform-app/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "medusa_ecr_repo" {
-  name = "medusa-backend"
+  name = "medusa-backend:latest"
   image_scanning_configuration { scan_on_push = true }
   image_tag_mutability = "MUTABLE"
 }
